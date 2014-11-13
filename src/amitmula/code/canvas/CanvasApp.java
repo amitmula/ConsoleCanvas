08577f3f6f9e3d8cb5177293cd3dff7a30b08c2e
Amit Mula <amit.mula@ampf.com>
2014-11-13 19:32:35 +0530

    removing unused class

--- src/amitmula/code/canvas/CanvasApp.java
+++ src/amitmula/code/canvas/CanvasApp.java
@@ -2,32 +2,6 @@ package amitmula.code.canvas;
 
 import java.util.Scanner;
 
-class SPoint {
-	public SPoint(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
-
-	int x,y;
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-}
-
 class Canvas {
 	char[][] canvasArray;
 	int w, h;
