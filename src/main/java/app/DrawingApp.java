4a90f10ffd1506abc6588d2255b436a9dc3a81e2
Amit Mula <amula@sapient.com>
2018-08-21 10:11:04 +0530

    added empty canvas check

--- src/main/java/app/DrawingApp.java
+++ src/main/java/app/DrawingApp.java
@@ -2,11 +2,14 @@ package app;
 
 import bean.Canvas;
 import exception.CanvasException;
-
 import java.util.Scanner;
 
 class DrawingApp {
+
     Canvas canvas;
+    public DrawingApp() {
+        canvas = new Canvas();
+    }
     public static void main(String[] args) throws NumberFormatException {
         DrawingApp app = new DrawingApp();
         Scanner scan = new Scanner(System.in);
@@ -32,28 +35,16 @@ class DrawingApp {
                     break;
                 case 'L' :
                     cmd = command.split(" ");
-                    if(this.canvas == null){
-                        System.err.println("Draw a canvas first");
-                        return;
-                    }
                     this.canvas.drawLine(Integer.parseInt(cmd[1]),Integer.parseInt(cmd[2]),Integer.parseInt(cmd[3]),Integer.parseInt(cmd[4]),'X');
                     System.out.println(this.canvas.render());
                     break;
                 case 'R' :
                     cmd = command.split(" ");
-                    if(this.canvas == null){
-                        System.err.println("Draw a canvas first");
-                        return;
-                    }
                     this.canvas.drawRectangle(Integer.parseInt(cmd[1]),Integer.parseInt(cmd[2]),Integer.parseInt(cmd[3]),Integer.parseInt(cmd[4]),'X');
                     System.out.println(this.canvas.render());
                     break;
                 case 'B' :
                     cmd = command.split(" ");
-                    if(this.canvas == null){
-                        System.err.println("Draw a canvas first");
-                        return;
-                    }
                     this.canvas.bucketFill(Integer.parseInt(cmd[1]),Integer.parseInt(cmd[2]),cmd[3].charAt(0));
                     System.out.println(this.canvas.render());
                     break;
--- src/main/java/bean/Canvas.java
+++ src/main/java/bean/Canvas.java
@@ -5,6 +5,7 @@ import exception.CanvasException;
 public class Canvas {
     char[][] canvasArray;
     int w, h;
+    public Canvas(){}
     public Canvas(int w, int h) {
         if(w < 1 || h < 1) {
             throw new CanvasException("Canvas width and height can't be 0");
@@ -20,33 +21,36 @@ public class Canvas {
         drawLine(this.w-1, 1, this.w-1, this.h-2, '|');
     }
     public String render() {
+        checkCanvas();
         StringBuilder strBuilder = new StringBuilder();
         for(int i=0;i<this.h;i++) {
             for(int j=0;j<this.w;j++) {
-                strBuilder.append(canvasArray[i][j] == '\u0000'?' ':canvasArray[i][j]);
+                strBuilder.append(this.canvasArray[i][j] == '\u0000'?' ':this.canvasArray[i][j]);
             }
             strBuilder.append("\n");
         }
         return strBuilder.toString().trim();
     }
-    public void drawLine(int x1, int y1, int x2, int y2, char mchar) {
+    public void drawLine(int x1, int y1, int x2, int y2, char mChar) {
+        checkCanvas();
         for(int i=y1; i<=y2; i++) {
             for(int j=x1; j<=x2; j++) {
-                this.canvasArray[i][j] = mchar;
+                this.canvasArray[i][j] = mChar;
             }
         }
     }
     public void drawRectangle(int x1, int y1, int x2, int y2, char mchar) {
+        checkCanvas();
         drawLine(x1,y1, x2, y1, mchar);
         drawLine(x1,y1, x1, y2, mchar);
         drawLine(x2,y1, x2, y2, mchar);
         drawLine(x1,y2, x2, y2, mchar);
     }
     public void bucketFill(int x, int y, char mchar) {
+        checkCanvas();
         if((int)this.canvasArray[y][x] != 0) {
             return;
         }
-
         if(x > 0 || x < this.h || y > 0 || y  < this.w) {
             if((int)this.canvasArray[y][x] == 0)
                 this.canvasArray[y][x] = mchar;
@@ -55,5 +59,10 @@ public class Canvas {
             bucketFill(x,y-1, mchar);
             bucketFill(x,y+1, mchar);
         }
+    }
+
+    private void checkCanvas() {
+        if(this.canvasArray == null)
+            throw new CanvasException("Draw a canvas first");
     }
 }
