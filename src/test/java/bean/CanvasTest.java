763a8d87b97c76864d06e1f66836b374a9d664e9
Amit Mula <amula@sapient.com>
2018-08-21 10:12:18 +0530

    added more tests

--- src/test/java/bean/CanvasTest.java
+++ src/test/java/bean/CanvasTest.java
@@ -27,15 +27,49 @@ class CanvasTest {
     }
 
     @Test
-    void testDrawLine() {
+    void testDrawLineException() {
+        Executable closureContainingCodeToTest = () -> new Canvas().drawLine(1,2,3,4,'x');
+        assertThrows(CanvasException.class, closureContainingCodeToTest, "");
+    }
 
+    @Test
+    void testDrawLine() {
+        Canvas canvas = new Canvas(3,5);
+        canvas.drawLine(1, 2, 1, 4, 'x');
+        assertEquals(canvas.render(),
+        "-----\n" +
+            "|   |\n" +
+            "|x  |\n" +
+            "|x  |\n" +
+            "|x  |\n" +
+            "|   |\n" +
+            "-----");
     }
 
     @Test
     void testDrawRectangle() {
+        Canvas canvas = new Canvas(8,4);
+        canvas.drawRectangle(2, 1, 6, 3, 'x');
+        assertEquals(canvas.render(),
+            "----------\n" +
+            "| xxxxx  |\n" +
+            "| x   x  |\n" +
+            "| xxxxx  |\n" +
+            "|        |\n" +
+            "----------");
     }
 
     @Test
     void testBucketFill() {
+        Canvas canvas = new Canvas(8,4);
+        canvas.drawRectangle(2, 1, 6, 3, 'x');
+        canvas.bucketFill(3,2, 'o');
+        assertEquals(canvas.render(),
+    "----------\n" +
+            "| xxxxx  |\n" +
+            "| xooox  |\n" +
+            "| xxxxx  |\n" +
+            "|        |\n" +
+            "----------");
     }
 }
